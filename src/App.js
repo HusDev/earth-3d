6847352c9ae32b4cd86965659d8fda7033bd3768
hussein.hasan <hussein.hasan@leaguesofcode.com>
2021-11-30 22:29:59 +0100

    feat: add canvas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import { Earth } from './components/earth';
+
+const CanvasContainer = styled.div`
+  width:100%;
+  height: 100%;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <CanvasContainer>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Earth />
+        </Suspense>
+      </Canvas>
+   </CanvasContainer>
   );
 }
 
